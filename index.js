6763953662af28b2e54409b01ff8acd3bcf5dca0
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-13 17:50:08 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//Random Password Generator
+
+function generatePassword(
+  passwordlength,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  return password;
+}
+
+const passwordlength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+const res = generatePassword(
+  passwordlength,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+);
+console.log(res);
